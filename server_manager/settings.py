44bdd3901370b92cfce9e53311fafddc71991ef4
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2021-11-20 00:55:49 +0530

    remove usage_dashboard app

--- server_manager/settings.py
+++ server_manager/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "unlock_keyring",
     "dashboard",
     "usage_api",
-    "usage_dashboard",
 ]
 
 MIDDLEWARE = [
--- usage_dashboard/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def testing(request):
-
-    return render(request, "index1.html")
